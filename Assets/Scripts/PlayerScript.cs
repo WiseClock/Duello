a1871d5c02c664e0c631b7e92f9fb365905dd2cb
Ryan Dieno <ryan.dieno@gmail.com>
2017-10-11 14:18:19 -0700

    Added player movement and jumping. Added simple platform.

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    public float movementSpeed;
+
+    public Transform[] groundPoints;
+    private float groundRadius = 0.2f;
+    public LayerMask groundLayerMask;
+    private bool grounded;
+
+    private bool jumping;
+    private float jumpTakeOffSpeed = 400.0f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+	}
+
+    void Update()
+    {
+        HandleInput();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+
+        float horizontal = Input.GetAxis("Horizontal");
+
+        grounded = IsGrounded();
+        HandleMovement(horizontal);
+
+        ResetValues();
+    }
+
+    private void HandleMovement(float horizontal)
+    {
+        rb.velocity = new Vector2(horizontal * movementSpeed, rb.velocity.y);
+        
+        if(grounded && jumping)
+        {
+            grounded = false;
+            rb.AddForce(new Vector2(0.0f, jumpTakeOffSpeed));
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            if (rb.velocity.y > 0)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            }
+        }
+    }
+
+    private void HandleInput()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            jumping = true;
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        if(rb.velocity.y <= 0)
+        {
+            foreach(Transform point in groundPoints)
+            {
+                Collider2D[] colliders = Physics2D.OverlapCircleAll(point.position, groundRadius, groundLayerMask);
+
+                for(int i = 0; i < colliders.Length; i++)
+                {
+                    if(colliders[i].gameObject != gameObject)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+
+    private void ResetValues()
+    {
+        jumping = false;
+    }
+}
