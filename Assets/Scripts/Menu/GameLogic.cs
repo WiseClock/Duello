cd3d0a024ae2ab8eab1098713177737e7468f7b8
WiseClock <kuanghao04@gmail.com>
2017-10-13 20:24:07 -0700

    Changed menu layout, added basic menu game logic.

---
+++ Assets/Scripts/Menu/GameLogic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour
+{
+    private GameObject _menu;
+    private GameObject _menuSelection;
+
+	void Start ()
+	{
+		_menu = GameObject.Find("MenuCanvas");
+	}
+
+	void Update ()
+	{
+	    float moveHorizontal = Input.GetAxis("Horizontal");
+	    float moveVertical = Input.GetAxis("Vertical");
+	    
+	    Camera.main.transform.eulerAngles = new Vector3(moveVertical * 5, moveHorizontal * -5, 0);
+        _menu.transform.eulerAngles = new Vector3(moveVertical * -5, moveHorizontal * 5, 0);
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
