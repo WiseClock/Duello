411aab3d9ae0cd255de3d60ea08b6fbfcfe6b5f3
Matt Goerwell <Superninja246@hotmail.com>
2017-10-16 11:49:23 -0700

    Basic hitbox and atack detection. Creation of knockback and damage functions.

---
+++ Assets/Scripts/DamageHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageHandler : MonoBehaviour {
+
+    public Rigidbody2D body;
+    public Rigidbody2D oppponent;
+	// Use this for initialization
+	void Start () {
+        body = gameObject.GetComponent<Rigidbody2D>();
+        oppponent = gameObject.GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TakeDamage(float damage) {
+        Debug.Log("Took damage: " + damage);
+    }
+
+    public void TakeKnockback(float knockback) {
+        Vector2 direction = oppponent.position - body.position;
+        direction.Normalize();
+        ForceMode2D mode = ForceMode2D.Impulse;
+        body.AddForce(direction * knockback, mode);
+        Debug.Log(direction);
+        
+    }
+}
---
+++ Assets/Scripts/FighterScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterScript : MonoBehaviour {
+
+    private float damage = 0.0f;
+    public Collider[] attacks;
+    private float knockback = 50.0f;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1")) {
+            damage = 10f;
+            attack(attacks[0]);
+        }
+	}
+
+    void attack(Collider col) {
+        Collider[] cols = Physics.OverlapBox(col.bounds.center,col.bounds.extents,col.transform.rotation,LayerMask.GetMask("Hitbox"));
+        foreach(Collider c in cols) {
+            if (c.transform.root == transform){
+                continue;
+            }
+            c.SendMessageUpwards("TakeDamage", damage);
+            c.SendMessageUpwards("TakeKnockback", knockback);
+            
+        }
+    }
+}
