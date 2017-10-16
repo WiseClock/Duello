69884f115a15810ccd8e9ed31016df759ca95739
Matt Goerwell <Superninja246@hotmail.com>
2017-10-16 12:04:37 -0700

    Implemented knockback function

--- Assets/Scripts/DamageHandler.cs
+++ Assets/Scripts/DamageHandler.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class DamageHandler : MonoBehaviour {
 
-    public Rigidbody2D body;
-    public Rigidbody2D oppponent;
+    
+    public GameObject Opponent;
+    private Rigidbody2D body;
+    private Rigidbody2D oppponent;
 	// Use this for initialization
 	void Start () {
         body = gameObject.GetComponent<Rigidbody2D>();
-        oppponent = gameObject.GetComponent<Rigidbody2D>();
+        oppponent = Opponent.GetComponent<Rigidbody2D>();
     }
 	
 	// Update is called once per frame
@@ -22,10 +24,12 @@ public class DamageHandler : MonoBehaviour {
     }
 
     public void TakeKnockback(float knockback) {
+        Debug.Log("Body position: " + body.position);
+        Debug.Log("Opponent position: " + oppponent.position);
         Vector2 direction = oppponent.position - body.position;
         direction.Normalize();
         ForceMode2D mode = ForceMode2D.Impulse;
-        body.AddForce(direction * knockback, mode);
+        body.AddForce(direction * -knockback, mode);
         Debug.Log(direction);
         
     }
