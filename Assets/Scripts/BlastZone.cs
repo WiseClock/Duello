ec49ab2506edc81d81068445e3ecf76ce4045601
Matt Goerwell <Superninja246@hotmail.com>
2017-10-16 17:52:51 -0700

    implementing kill field at bottom of map

--- Assets/Scripts/BlastZone.cs
+++ Assets/Scripts/BlastZone.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BlastZone : MonoBehaviour {
-
+    //handling for the killfield below the map.
     private float damage = 100;
     private Collider self;
 
