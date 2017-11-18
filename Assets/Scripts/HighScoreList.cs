28a0c6b540a04f361515e2ca53711b3b232369d8
Robert B <rbroyles@my.bcit.ca>
2017-11-18 03:56:32 -0800

    Updated ScoreManager
    I added a new scene called HighScoreScene to show the top 5 scores, but it's not working as intended.
    
    Also, for some reason, all scores are changing to the top score in ScoreManager.cs, still needs work obviously. I haven't gotten initials in yet.

---
+++ Assets/Scripts/HighScoreList.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScoreList : MonoBehaviour {
+
+    private int[] highScores = { 0, 0, 0, 0, 0 };
+    public Text highScoreText;
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 1; i <= 5; i++)
+        {
+            string highScore = "highscore" + i.ToString();
+            highScores[i-1] = PlayerPrefs.GetInt(highScore, 0);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        highScoreText.text = "CURRENT HIGH SCORES\n"
+                              + "\n" + highScores[0]
+                              + "\n" + highScores[1]
+                              + "\n" + highScores[2]
+                              + "\n" + highScores[3]
+                              + "\n" + highScores[4];
+    }
+}
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -6,14 +6,25 @@ public class ScoreManager : MonoBehaviour {
     public static int healthScore;
     public static int timerScore;
     public static int totalScore;
+    private string highScoreTag;
+    //private string highScorerTag;
     private int highScore;
+    private int checkHighScore;
+    //private string highScorer;
     public Text scoreText;
     public Text highScoreText;
 
     void Awake()
     {
         // Get current high score
-        highScore = PlayerPrefs.GetInt("highscore", 0);
+        //highScore = PlayerPrefs.GetInt("highscore1", 0);
+        //highScorer = PlayerPrefs.GetString("highscorer1", "AAA");
+
+        /*PlayerPrefs.SetInt("highscore1", 0);
+        PlayerPrefs.SetInt("highscore2", 0);
+        PlayerPrefs.SetInt("highscore3", 0);
+        PlayerPrefs.SetInt("highscore4", 0);
+        PlayerPrefs.SetInt("highscore5", 0);*/
 
         // Reset the scores
         healthScore = 0;
@@ -25,13 +36,28 @@ public class ScoreManager : MonoBehaviour {
     void Update()
     {
         totalScore = healthScore + timerScore;
+        int currentScore = totalScore;
 
-        // Check for a new high score, set if needed
-        if (totalScore > highScore)
+        // Go through all saved high scores
+        for (int i = 1; i <= 5; i++)
         {
-            PlayerPrefs.SetInt("highscore", totalScore);
-        }
+            // Get High Score
+            highScoreTag = "highscore" + i.ToString();
+            checkHighScore = PlayerPrefs.GetInt(highScoreTag, 0);
 
+            // Get High Scorer Initials
+            //highScorerTag = "highscorer" + i;
+            //highScorer = PlayerPrefs.GetString(highScorerTag);
+
+            // Check for a new high score, set if needed
+            if (currentScore > checkHighScore)
+            {
+                PlayerPrefs.SetInt(highScoreTag, currentScore);
+                //PlayerPrefs.SetString(highScorerTag, tempScorer);
+                currentScore = checkHighScore;
+            }
+        }
+        
         // Set the display text when the game ends
         if (totalScore > 0)
         {
@@ -48,7 +74,11 @@ public class ScoreManager : MonoBehaviour {
         }
 
         // Display the current high score
+        highScore = PlayerPrefs.GetInt("highscore1");
         highScoreText.text = "CURRENT HIGH SCORE: " + highScore;
-        
+        /*highScoreText.text = string.Format("{0} {1} {2,10}"
+                                        , "CURRENT HIGH SCORE:"
+                                        , highScorer
+                                        , highScore);*/
     }
 }
