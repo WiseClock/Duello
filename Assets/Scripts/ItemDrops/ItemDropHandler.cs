4a16dea73e7ff2e452fa48b3acc25290958ceb01
WiseClock <kuanghao04@gmail.com>
2017-11-19 22:29:36 -0800

    working item drop function.

---
+++ Assets/Scripts/ItemDrops/ItemDropHandler.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDropHandler : MonoBehaviour
+{
+    private GameObject _player;
+    private GameObject _enemy;
+    private GameObject _blastZone;
+    private int _angle = 0;
+    private int _rotateDirection = 1;
+    private string _type;
+
+    private Action _onDestroyCallback;
+    private Action<GameObject> _onCollisionCallback;
+
+	void Start ()
+    {
+        _player = GameObject.Find("Player");
+        _enemy = GameObject.Find("Enemy");
+        _blastZone = GameObject.Find("BlastZone");
+        
+        gameObject.layer = 31;
+        Physics2D.IgnoreLayerCollision(gameObject.layer, gameObject.layer);
+    }
+
+    void Update ()
+    {
+		transform.Rotate(new Vector3(0, 1, 0), _rotateDirection);
+        _angle += _rotateDirection;
+        if (_angle == 20 || _angle == -20)
+            _rotateDirection *= -1;
+    }
+
+    public void SetType(string materialName)
+    {
+        _type = materialName;
+        Material newMat = Resources.Load<Material>("Materials/ItemDrops/" + materialName);
+        GetComponent<Renderer>().material = newMat;
+
+        switch (materialName)
+        {
+            case "Speed":
+                _onCollisionCallback = o =>
+                {
+                    Debug.Log(o.name + " gets the " + _type + " buff!");
+                };
+                break;
+            case "Damage":
+                _onCollisionCallback = o =>
+                {
+                    Debug.Log(o.name + " gets the " + _type + " buff!");
+                };
+                break;
+            case "Jump":
+                _onCollisionCallback = o =>
+                {
+                    Debug.Log(o.name + " gets the " + _type + " buff!");
+                };
+                break;
+            case "Regeneration":
+                _onCollisionCallback = o =>
+                {
+                    Debug.Log(o.name + " gets the " + _type + " buff!");
+                };
+                break;
+            case "Resistance":
+                _onCollisionCallback = o =>
+                {
+                    Debug.Log(o.name + " gets the " + _type + " buff!");
+                };
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void SetOnDestroyCallback(Action action)
+    {
+        _onDestroyCallback = action;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject targetObject = collision.gameObject;
+
+        if (targetObject != _player && targetObject != _enemy && targetObject != _blastZone)
+            return;
+
+        if (targetObject == _player || targetObject == _enemy)
+            _onCollisionCallback(collision.gameObject);
+
+        Destroy(gameObject);
+        _onDestroyCallback();
+    }
+}
--- Assets/Scripts/ItemDrops/ItemDropTicker.cs
+++ Assets/Scripts/ItemDrops/ItemDropTicker.cs
@@ -4,13 +4,42 @@ using UnityEngine;
 
 public class ItemDropTicker : MonoBehaviour
 {
-	void Start ()
+    public float RangeXMin = -8;
+    public float RangeXMax = 8;
+    public int MaxAllowedItems = 3;
+    public int CheckIntervalSeconds = 3;
+    public float Possibility = 0.3f;
+
+    private int _currentItemCount = 0;
+    private int _lastGeneratedTime = -1;
+
+    private static readonly string[] ITEM_TYPES = { "Speed", "Damage", "Jump", "Regeneration", "Resistance" };
+
+    void Start ()
 	{
 		
 	}
 
-	void Update ()
+	void FixedUpdate ()
 	{
-		
+	    int timeInSeconds = (int)Time.realtimeSinceStartup;
+
+        if (_currentItemCount < MaxAllowedItems && timeInSeconds > _lastGeneratedTime && timeInSeconds % CheckIntervalSeconds == 0 && Random.value < Possibility)
+	    {
+	        _currentItemCount++;
+	        _lastGeneratedTime = timeInSeconds;
+            float xPos = Random.Range(RangeXMin, RangeXMax);
+            GameObject itemDrop = (GameObject)Instantiate(Resources.Load("Prefabs/ItemDrops/ItemDrop"));
+            itemDrop.transform.position = new Vector3(xPos, 10, 0);
+	        ItemDropHandler handler = itemDrop.GetComponent<ItemDropHandler>();
+            handler.SetType(ITEM_TYPES[Mathf.FloorToInt((ITEM_TYPES.Length - 1) * Random.value)]);
+            handler.SetOnDestroyCallback(OnItemDropDestroyCallback);
+            Debug.Log("dropping item");
+	    }
 	}
+
+    private void OnItemDropDestroyCallback()
+    {
+        _currentItemCount--;
+    }
 }
